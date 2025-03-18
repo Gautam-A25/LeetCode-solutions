570fe78f76c1f507faf77b043f7e74e27fa08fb0
Aayush <aayushgautam2502@gmail.com>
2025-03-18 21:29:23 +0530

    Add: HashMap_Solution

---
+++ HashMap/#2206.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool divideArray(vector<int>& nums) {
+        unordered_map<int, int> map;
+
+        for(auto i : nums){
+            map[i]++;
+        }
+
+        for(auto i : map){
+            if(i.second%2!=0) return false;
+        }
+        return true;
+    }
+};
