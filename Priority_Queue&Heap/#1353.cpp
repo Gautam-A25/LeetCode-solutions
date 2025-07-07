2c7ad2b17379aa3f7da6e7b8e6ae055ee4ff250e
Gautam-A25 <aayushgautam2502@gmail.com>
2025-07-08 04:06:12 +0530

    Add: Min Heap Solution

---
+++ Priority_Queue&Heap/#1353.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int maxEvents(vector<vector<int>>& events) {
+        sort(events.begin(), events.end());
+        priority_queue<int, vector<int>, greater<int>> minHeap;
+
+        int day = 0, index = 0, n = events.size(), count = 0;
+
+        while(!minHeap.empty() || index < n){
+            if(minHeap.empty()){
+                day = events[index][0];
+            }
+            while(index < n && events[index][0] <= day){
+                minHeap.push(events[index][1]);
+                index++;
+            }
+            minHeap.pop();
+            count++;
+            day++;
+            while(!minHeap.empty() && minHeap.top() < day){
+                minHeap.pop();
+            }
+        }
+        return count;
+    }
+};
